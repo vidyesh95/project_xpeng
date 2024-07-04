9a264aafac59d53a476250abf45baee6a344a3f8
Vidyesh Churi <vidyesh95@gmail.com>
2024-07-05 01:05:58 +0530

    added javascript

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 var msg = document.getElementById("msg");
 var storeduname = localStorage.getItem("uname");
 msg.innerHTML = "Hi, " + storeduname;
+
+const form = document.getElementById("calculate");
+form.addEventListener("submit", function () {
+    event.preventDefault();
+    const fromcurrency = "USD";
+    const tocurrency = "INR";
+    const amount = Number(40000);
+    const result = document.getElementById("result");
+    let rate;
+    fetch(`https://api.exchangerate-api.com/v4/latest/${fromcurrency}`)
+        .then((response) => response.json())
+        .then((data) => {
+            rate = data.rates[tocurrency];
+            const answer = amount * rate;
+            result.innerHTML =
+                amount +
+                " " +
+                fromcurrency +
+                " is equal to " +
+                answer +
+                " " +
+                tocurrency;
+            //console.log(answer);
+        });
+});
